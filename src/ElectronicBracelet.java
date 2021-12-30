9a222c68454896afa811b9d64fc199b9ba6df1cf
Jonathan-gr <53814546+Jonathan-gr@users.noreply.github.com>
2021-12-30 17:23:42 +0200

    Add files via upload

--- src/ElectronicBracelet.java
+++ src/ElectronicBracelet.java
@@ -1,3 +1,53 @@
-public class ElectronicBracelet
-{
+import java.util.ArrayList;
+public class ElectronicBracelet {
+    private String bracelet_id;
+    private Kid kid;
+    private eTicket eTicket;
+
+    public ElectronicBracelet(String bracelet_id, Kid kid, eTicket eTicket) {
+        this.bracelet_id = bracelet_id;
+        this.kid = kid;
+        this.eTicket = eTicket;
+    }
+
+    public eTicket geteTicket() {
+        return eTicket;
+    }
+
+    public void seteTicket(eTicket eTicket) {
+        this.eTicket = eTicket;
+    }
+
+    public Kid getKid() {
+        return kid;
+    }
+
+    public void setKid(Kid kid) {
+        this.kid = kid;
+    }
+
+    public String getBracelet_id() {
+        return bracelet_id;
+    }
+
+    public void setBracelet_id(String bracelet_id) {
+        this.bracelet_id = bracelet_id;
+    }
+
+
+    public ArrayList getKidInformation() {
+        int ID = Integer.parseInt(kid.getID());
+        int height = kid.getHeight();
+        int weight = kid.getWeight();
+        int age = kid.getAge();
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(ID);
+        list.add(height);
+        list.add(weight);
+        list.add(age);
+        return list;
+
+    }
+
 }
